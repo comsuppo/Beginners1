2e17363c8410428dd040a37fb6143c146799583d
“midomido213” <midopoppo213@gmail.com>
2017-09-21 16:47:58 +0900

    データベースから記事を読み込んでテーブル表示

--- article.php
+++ article.php
@@ -6,6 +6,9 @@ $password = "";
 $database = "beginners";
 $dbport = 3306;
 
+// フォームカウント用
+$num = 1;
+
 // mySQLに接続
 $db = new mysqli($servername, $username, $password, $database, $dbport);
 $db->set_charset('utf8');
@@ -15,4 +18,40 @@ if($db->connect_error){
     die("Connection failed: " . $db->connect_error);
 }
 
+// データベースから降順で読み込み
+$result = $db->query("select * from `article` order by `id` desc");
+
 ?>
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <h1>Beginners BBS</h1>
+        <table border="1">
+            <caption>記事一覧</caption>
+            <tr style="background:#BDBDBD">
+                <th>記事名</th>
+                <th>作成時間</th>
+            </tr>
+            <?php foreach($result as $row) : ?>
+            <tr>
+                <?php
+                $title = htmlspecialchars($row['title']);
+                $time = htmlspecialchars($row['time']);
+                ?>
+                <td>
+                    <form action="board.php" method="post" name="form<?php echo $num ?>">
+                        <a href="#" onclick="document.forms.form<?php echo $num ?>.submit()"><?php echo $title ?></a>
+                    </form>
+                </td>
+                <td>
+                    <?php echo $time ?>
+                </td>
+            </tr>
+            <?php $num += 1; ?>
+            <?php endforeach; ?>
+        </table>
+    </body>
+</html>
