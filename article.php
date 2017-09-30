ba9358d10e33eb56363650d2ba482c22a8270ad9
isjuku_slack <isjuku_slack@protonmail.com>
2017-09-30 16:06:13 +0900

    a

--- article.php
+++ article.php
@@ -26,11 +26,36 @@ $result = $db->query("select * from `article` where `flag` = '0' order by `id` d
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8" />
-        <title>Beginners BBS</title>
-        <link rel="stylesheet" type="text/css" href="bbs_style.css">
-    </head>
+            <meta charset="utf-8" />
+            <title>Beginners BBS</title>
+            
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <!-- Bootstrap CSS -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+            <link rel="stylesheet" type="text/css" href="bbs_style.css">
+   </head>
     <body>
+        <nav class="navbar navbar-expand-md navbar-light bg-light rounded mb-3">
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav text-md-center nav-justified w-100">
+                          <li class="nav-item active">
+                            <a class="nav-link primary" href="article.php">掲示板</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="https://gitpitch.com/comsuppo/is2017_team_beginners">gitpitch</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="https://github.com/comsuppo/Beginners1">github</a>
+                          </li>
+                    </ul>
+              </div>
+        </nav>
+        
+        
+        
         <h1>Beginners BBS</h1>
         
         <div>
@@ -49,19 +74,23 @@ $result = $db->query("select * from `article` where `flag` = '0' order by `id` d
         $time = htmlspecialchars($row['time']);
         ?>
         
-        <div class="waku">
-            <div class="link">
-                <span>
+        <div class="container mb-4">
+            <div class="jumbotron">
+              <h1 class="display-3"><?php echo $title ?></h1>
+              <p class="lead"></p>
+              <hr class="my-4">
+              <textarea rows="4" cols="40"><?php echo $description ?></textarea>
+              <p style="word-wrap: break-word;"><?php echo $description ?></p>
+              <p class="lead">
                     <form action="board.php" method="post" name="form<?php echo $num ?>">
                         <input type="hidden" value="<?php echo $id ?>" name="article_id" />
                         <input type="hidden" value="<?php echo $title ?>" name="title" />
                         <input type="hidden" value="<?php echo $description ?>" name="description" />
-                        <a href="#" onclick="document.forms.form<?php echo $num ?>.submit()"><?php echo $title ?></a>
+                        <a  class="btn btn-primary btn-lg" href="#" role="button" href="#" onclick="document.forms.form<?php echo $num ?>.submit()">記事へ</a>
                     </form>
-                </span>
-                <br>
-                <span><?php echo $time ?></span>
+              </p>
             </div>
+            <span class="float-left"><?php echo $time ?></span>
             <div class="right">
                 <span>
                     <form action="delete_article.php" method="post">
@@ -75,6 +104,13 @@ $result = $db->query("select * from `article` where `flag` = '0' order by `id` d
         
         <?php $num += 1; ?>
         <?php endforeach; ?>
+        
+        
+        
+        
+        <footer class="footer mt-5">
+          <p class="bg-dark text-light text-center">&#169;2017 team beginners </p>
+        </footer>
         
     </body>
 </html>
